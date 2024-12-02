70d25c76511950f1e95de9de742773389aafdf56
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-12-02 23:12:43 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main()
+{
+    cout << "**     **"<< endl;
+    cout << "**    **"<< endl;
+    cout << "**   **"<< endl;
+    cout << "**  **"<< endl;
+    cout << "** **"<< endl;
+    cout << "****"<< endl;
+    cout << "** **"<< endl;
+    cout << "**  **"<< endl;
+    cout << "**   **"<< endl;
+    cout << "**    **"<< endl;
+    cout << "**     **"<< endl;
+    return 0;
+}
